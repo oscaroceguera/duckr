7c7db4dbce4df94208f11baf4f4fe42f7f649d1c
oscar <oscaroceguera@gmail.com>
2016-11-27 19:47:57 -0700

    URL State in Redux

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Router, hashHistory, IndexRoute } from 'react-router'
+import { Router, IndexRoute } from 'react-router'
 import {
   MainContainer, HomeContainer, AuthenticateContainer,
   FeedContainer, LogoutContainer, UserContainer, DuckDetailsContainer } from 'containers'
 
-export default function getRoutes (checkAuth) {
+export default function getRoutes (checkAuth, history) {
   return (
-    <Router history={hashHistory}>
+    <Router history={history}>
       <Router path='/' component={MainContainer}>
         <Router path='auth' component={AuthenticateContainer} onEnter={checkAuth}/>
         <Router path='feed' component={FeedContainer} onEnter={checkAuth}/>
